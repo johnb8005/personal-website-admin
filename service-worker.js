5c73f742ae2140e8859a1339826de3e691c6c11c
johnb8005 <johnb8005@users.noreply.github.com>
2021-04-30 10:11:20 +0000

    deploy: aa1f24f98f4d44ee0e6f6708df948e5c10efa2bf

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website-admin/precache-manifest.488815015cb9ac54f10140262310e9ac.js"
+  "/personal-website-admin/precache-manifest.2df6a1b31abd846c3d2d7affbf4427fd.js"
 );
 
 self.addEventListener('message', (event) => {
